54621bd2c415cf7753c2af4139cd54ef6c9e02c0
Leonardo Arellano Martinez <leo.aremtz98@gmail.com>
2025-11-09 02:17:50 -0600

    Add cluster issuer configuration

--- cluster-configuration/cert-manager/main.tf
+++ cluster-configuration/cert-manager/main.tf
@@ -7,6 +7,18 @@ terraform {
   }
 }
 
+variable "env" {
+  description = "Project Environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
 variable "cert_manager_values" {
   description = "Map of values that will be merged with the default values"
   type        = any
@@ -29,6 +41,11 @@ locals {
   merged_values = merge(local.default_values, var.cert_manager_values)
 }
 
+data "aws_ssm_parameter" "cloudflare_token" {
+  name = "/account-configuration/${var.env}/cluster-issuer-cloudflare-token"
+}
+
+
 resource "helm_release" "cert_manager" {
   name             = "cert-manager"
   namespace        = "cert-manager"
@@ -37,4 +54,49 @@ resource "helm_release" "cert_manager" {
   create_namespace = true
 
   values = [yamlencode(local.merged_values)]
+}
+
+resource "kubernetes_manifest" "cloudflare_token" {
+  manifest = {
+    apiVersion = "v1"
+    kind       = "Secret"
+    metadata = {
+      name = "cloudflare-api-token-secret"
+    }
+    type = "Opaque"
+    stringData = {
+      api-token = "${data.aws_ssm_parameter.cloudflare_token.value}"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "test-configmap" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "letsencrypt-staging"
+      #namespace = "default"
+    }
+    spec = {
+      acme = {
+        email  = "leo.aremtz98@gmail.com"
+        server = "https://acme-staging-v02.api.letsencrypt.org/directory"
+        privateKeySecretRef = {
+          name = "letsencrypt-staging-key"
+        }
+        solvers = [{
+          dns01 = {
+            cloudflare = {
+              email = "leo.aremtz98@gmail.com"
+              apiTokenSecretRef = {
+                name = kubernetes_manifest.cloudflare_token.metadata.name
+                key  = "api-token"
+              }
+            }
+          }
+        }]
+      }
+    }
+  }
 }
