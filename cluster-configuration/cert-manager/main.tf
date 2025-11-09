881eeae43a876d289c3dc9636d6cadf794b425dc
Leonardo Arellano Martinez <leo.aremtz98@gmail.com>
2025-11-09 02:42:05 -0600

    Set version for aws and kubernetes providers

--- cluster-configuration/cert-manager/main.tf
+++ cluster-configuration/cert-manager/main.tf
@@ -1,5 +1,13 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.98.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.38.0"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "3.1.0"
