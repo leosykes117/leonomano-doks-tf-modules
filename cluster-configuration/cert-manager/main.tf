db368b9d42e8d4a4a5a7d6dc89f0a0e068aa399a
Leonardo Arellano Martinez <leo.aremtz98@gmail.com>
2025-11-11 17:32:07 -0600

    Add terraform module to configure k8s objects to cluster tools.
    * Add a terraform module to configure external secrets operator.
    * Add a terraform module to configure cert-manager

--- cluster-configuration/cert-manager/main.tf
+++ cluster-configuration/cert-manager/main.tf
@@ -1,16 +1,8 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.98.0"
-    }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "2.38.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "3.1.0"
     }
   }
 }
@@ -21,67 +13,43 @@ variable "env" {
   default     = "dev"
 }
 
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "cert_manager_values" {
-  description = "Map of values that will be merged with the default values"
-  type        = any
-  default     = {}
-}
-
-provider "helm" {
-  kubernetes = {
-    config_path = "~/.kube/config"
-  }
-}
-
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "do-sfo2-dev-leonomano-projects"
 }
 
 locals {
-  namespace = "cert-manager"
-  default_values = {
-    crds = {
-      enabled = true
-    }
-  }
-  cloudflare_token = sensitive(base64encode(data.aws_ssm_parameter.cloudflare_token.value))
-
-  merged_values = merge(local.default_values, var.cert_manager_values)
+  namespace                                         = "cert-manager"
+  external_secret_cloudflare_token_name             = "${var.env}-clusterissuer-solver-cloudflare-token"
+  external_secrets_cloudflare_token_secret_key_name = "api-token"
 }
 
-data "aws_ssm_parameter" "cloudflare_token" {
-  name = "/account-configuration/${var.env}/cluster-issuer-cloudflare-token"
-}
-
-
-resource "helm_release" "cert_manager" {
-  name             = "cert-manager"
-  namespace        = local.namespace
-  chart            = "oci://quay.io/jetstack/charts/cert-manager"
-  version          = "v1.19.1"
-  create_namespace = true
-
-  values = [yamlencode(local.merged_values)]
-}
-
-resource "kubernetes_manifest" "cloudflare_token" {
+resource "kubernetes_manifest" "external-secret-cloudflare-token" {
   manifest = {
-    apiVersion = "v1"
-    kind       = "Secret"
+    apiVersion = "external-secrets.io/v1"
+    kind       = "ExternalSecret"
     metadata = {
-      name      = "cloudflare-api-token-secret"
+      name      = local.external_secret_cloudflare_token_name
       namespace = local.namespace
     }
-    type = "Opaque"
-    data = {
-      api-token = local.cloudflare_token
+    spec = {
+      refreshInterval = "24h"
+      secretStoreRef = {
+        kind = "ClusterSecretStore"
+        name = "aws-dev-parameter-store"
+      }
+      target = {
+        name           = local.external_secret_cloudflare_token_name
+        deletionPolicy = "Delete"
+      }
+      data = [
+        {
+          secretKey = local.external_secrets_cloudflare_token_secret_key_name
+          remoteRef = {
+            key = "/account-configuration/${var.env}/cluster-issuer-cloudflare-token"
+          }
+        }
+      ]
     }
   }
 }
@@ -92,7 +60,6 @@ resource "kubernetes_manifest" "staging-cluster-issuer" {
     kind       = "ClusterIssuer"
     metadata = {
       name = "letsencrypt-staging"
-      #namespace = "default"
     }
     spec = {
       acme = {
@@ -106,8 +73,8 @@ resource "kubernetes_manifest" "staging-cluster-issuer" {
             cloudflare = {
               email = "leo.aremtz98@gmail.com"
               apiTokenSecretRef = {
-                name = kubernetes_manifest.cloudflare_token.object.metadata.name
-                key  = "api-token"
+                name = local.external_secret_cloudflare_token_name
+                key  = local.external_secrets_cloudflare_token_secret_key_name
               }
             }
           }
